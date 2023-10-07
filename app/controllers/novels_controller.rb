c1a82ee1832c79dd80bd9615bbf62d818aa700d1
tabula9 <fhmtt484@gmail.com>
2023-10-08 03:58:59 +0900

    ストロングパラメータ実装

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -5,18 +5,23 @@ class NovelsController < ApplicationController
 
   def search
     narou_api = 'https://api.syosetu.com/novelapi/api/?out=json&'
-    if params[:genre].present?
-      genres = params[:genre].join('-')
-      request = URI.encode_www_form({ word: params[:word], order: params[:order], genre: genres })
-    elsif params[:word].present?
-      request = URI.encode_www_form({ word: params[:word], order: params[:order] })
+    @search_params = novel_search_params
+    if @search_params[:genre].present?
+      genres = @search_params[:genre].join('-')
+      request = URI.encode_www_form({ word: @search_params[:word], order: @search_params[:order], genre: genres })
     else
-      request = URI.encode_www_form(order: params[:order])
+      request = URI.encode_www_form({ word: @search_params[:word], order: @search_params[:order] })
     end
     narou_api += request
     @api = narou_api
     uri = URI.parse(narou_api)
     json = Net::HTTP.get(uri)
     @hash = JSON.parse(json)
+  end
+
+  private
+
+  def novel_search_params
+    params.fetch(:search, {}).permit(:word, :order, genre: [])
   end
 end
