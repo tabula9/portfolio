e38cc8ba65ed0a9b7f37e277b74de14bf3afebd5
tabula9 <fhmtt484@gmail.com>
2023-10-10 18:37:46 +0900

    空白をエンコードする際、＋ではなく%20になるよう修正

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -2,12 +2,17 @@ class NovelsController < ApplicationController
   require 'net/http'
   require 'json'
   require 'uri'
+  require "erb"
+  include ERB::Util
 
   def search
     narou_api = 'https://api.syosetu.com/novelapi/api/?out=json&'
     @search_params = novel_search_params
+    if @search_params[:word].present?
+      narou_api += "word=#{url_encode(@search_params[:word])}&"
+    end
     genres = Array(@search_params[:genre]).join('-')
-    request = URI.encode_www_form({ word: @search_params[:word], order: @search_params[:order], genre: genres })
+    request = URI.encode_www_form({ order: @search_params[:order], genre: genres })
     narou_api += request
     @api = narou_api
     uri = URI.parse(narou_api)
