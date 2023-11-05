dccede6511e4726d44c06026f07977e49f37724c
tabula9 <fhmtt484@gmail.com>
2023-11-06 03:37:45 +0900

    jqueryを導入してアコーディオンメニューを作成

---
+++ app/javascript/packs/test.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function() {
+    $(".test").text("成功");
+});
+
+$(document).on('turbolinks:load', function() {
+  $(function(){
+    $('.js-accordion-title').on('click', function () {
+      /*クリックでコンテンツを開閉*/
+      $(this).next().slideToggle(200);
+      /*矢印の向きを変更*/
+      $(this).toggleClass('open', 200);
+    });
+  });
+});
