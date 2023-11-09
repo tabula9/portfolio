0fb09816441484cc83cd1e4c4e3183bddf2a25c9
tabula9 <fhmtt484@gmail.com>
2023-11-10 06:10:00 +0900

    新規登録やログイン、ログアウトができるようリンク設置。nameカラムを追加して登録できるように設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_08_092916) do
+ActiveRecord::Schema.define(version: 2023_11_09_191947) do
 
   create_table "buntais", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2023_11_08_092916) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
