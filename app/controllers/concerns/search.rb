ca3a3158b74a7c6ccda043792064f5bf21a7d730
tabula9 <fhmtt484@gmail.com>
2023-11-25 12:30:00 +0900

    複数のコントローラーで使えるようurlエンコード処理をconcernsファイルに記述

---
+++ app/controllers/concerns/search.rb
@@ -0,0 +1,23 @@
+module Search
+  extend ActiveSupport::Concern
+
+  def encode
+    narou_api = 'https://api.syosetu.com/novelapi/api/?out=json&'
+    if @search_params[:word].present?
+      narou_api += "word=#{url_encode(@search_params[:word])}&"
+    end
+    if @search_params[:notword].present?
+      narou_api += "notword=#{url_encode(@search_params[:notword])}&"
+    end
+    genre_or = Array(@search_params[:genre]).join('-')
+    notgenre_or = Array(@search_params[:notgenre]).join('-')
+    buntai_or = Array(@search_params[:buntai]).join('-')
+    kaiwaritu = Array(@search_params[:kaiwaritu_min]).concat(Array(@search_params[:kaiwaritu_max])).uniq.join('-')
+    request = URI.encode_www_form({ order: @search_params[:order], stop: @search_params[:stop], kaiwaritu: kaiwaritu, genre: genre_or, notgenre: notgenre_or, buntai: buntai_or })
+    narou_api += request
+    @api = narou_api
+    uri = URI.parse(narou_api)
+    json = Net::HTTP.get(uri)
+    @hash = JSON.parse(json)
+  end
+end
--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -4,26 +4,15 @@ class NovelsController < ApplicationController
   require 'uri'
   require "erb"
   include ERB::Util
+  before_action :set_params
+  include Search
+  before_action :encode
+
+  def set_params
+    @search_params = novel_search_params
+  end
 
   def search
-    narou_api = 'https://api.syosetu.com/novelapi/api/?out=json&'
-    @search_params = novel_search_params
-    if @search_params[:word].present?
-      narou_api += "word=#{url_encode(@search_params[:word])}&"
-    end
-    if @search_params[:notword].present?
-      narou_api += "notword=#{url_encode(@search_params[:notword])}&"
-    end
-    genre_or = Array(@search_params[:genre]).join('-')
-    notgenre_or = Array(@search_params[:notgenre]).join('-')
-    buntai_or = Array(@search_params[:buntai]).join('-')
-    kaiwaritu = Array(@search_params[:kaiwaritu_min]).concat(Array(@search_params[:kaiwaritu_max])).uniq.join('-')
-    request = URI.encode_www_form({ order: @search_params[:order], stop: @search_params[:stop], kaiwaritu: kaiwaritu, genre: genre_or, notgenre: notgenre_or, buntai: buntai_or })
-    narou_api += request
-    @api = narou_api
-    uri = URI.parse(narou_api)
-    json = Net::HTTP.get(uri)
-    @hash = JSON.parse(json)
     @genres = Genre.all
     @orders = Order.all
     @buntais = Buntai.all
